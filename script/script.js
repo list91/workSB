fb7a9836ffd4d421f12d212b508b26f20646fbbd
Stas <s_anufriev@list.ru>
2023-06-29 13:09:39 +0700

    обновил стили и структуру файла скриптов JфS

--- script/script.js
+++ script/script.js
@@ -1,30 +1,25 @@
 
 // Calculator
-var calc_button  = document.getElementById('calc_button');
+//var calc_button  = document.getElementById('calc_button');
 
-function launch_calc()
-{
-   var oShell = new ActiveXObject("WScript.Shell");
-   oShell.Run('"' +'C:\\Windows\\System32\\calc.exe'  + '"', 1);
-}
-calc_button.addEventListener("click", launch_calc);
+//function launch_calc()
+//{
+//   var oShell = new ActiveXObject("WScript.Shell");
+//   oShell.Run('"' +'C:\\Windows\\System32\\calc.exe'  + '"', 1);
+//}
+//calc_button.addEventListener("click", launch_calc);
 
 
 
 // 1c
-var one_c_button = document.getElementById('1c_button');
+//var one_c_button = document.getElementById('1c_button');
 
-function launch_1c(){
-  var oShell = new ActiveXObject("WScript.Shell");
-  oShell.Run('"' +'C:\\Bases\\БСП\\БСП.v8i'  + '"', 1);
-}
+//function launch_1c(){
+//  var oShell = new ActiveXObject("WScript.Shell");
+//  oShell.Run('"' +'C:\\Bases\\БСП\\БСП.v8i'  + '"', 1);
+//}
 
-one_c_button.addEventListener("click", launch_1c);
-
-
-
-
-
+//one_c_button.addEventListener("click", launch_1c);
 
 // help button
 var help_button = document.getElementById('help_button');
@@ -46,3 +41,25 @@ var off_button = document.getElementById('off_button');
 // notification
 
 var notification_button = document.getElementById('notification_button');
+// <!-- START xtdyfzfiwklvwqrdzb -->
+// конвертер
+var button_xtdyfzfiwklvwqrdzb  = document.getElementById('xtdyfzfiwklvwqrdzb');
+
+function launch_xtdyfzfiwklvwqrdzb()
+{
+   var oShell = new ActiveXObject("WScript.Shell");
+   oShell.Run('"' +'C:\\Users\\Stas\\Desktop\\Конвертер YouTube в MP3_v1.exe'  + '"', 1);
+}
+button_xtdyfzfiwklvwqrdzb.addEventListener("click", launch_xtdyfzfiwklvwqrdzb);
+// <!-- END xtdyfzfiwklvwqrdzb -->
+// <!-- START bhexitkkmsrdhsnniy -->
+// wadw
+var button_bhexitkkmsrdhsnniy  = document.getElementById('bhexitkkmsrdhsnniy');
+
+function launch_bhexitkkmsrdhsnniy()
+{
+   var oShell = new ActiveXObject("WScript.Shell");
+   oShell.Run('"' + 'C:\\Users\\Stas\\Documents\\aw.xml'  + '"', 1);
+}
+button_bhexitkkmsrdhsnniy.addEventListener("click", launch_bhexitkkmsrdhsnniy);
+// <!-- END bhexitkkmsrdhsnniy -->
